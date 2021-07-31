f3b3835c453af88b23d86fdd5d38d44e1b94a058
Asim Ali <asimsali95@gmail.com>
2021-07-31 12:05:06 -0500

    commit tree 3851a792f5f311a9ca6a1cea9dab755d1103a7fc

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,27 @@
 # Add your code here
+class Dog
+    attr_accessor :name
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @save = save
+    end
+
+    def self.all #class method, reader for @@all class variable makes accesible outside of class
+        @@all
+    end
+
+    def self.clear_all #class emthod
+        @@all.clear
+    end
+
+    def save
+        @@all << self
+    end
+
+    def self.print_all #class method
+        @@all.each { |dog| puts "#{dog.name}" } # .loop {|element| iteration on element}
+    end
+
+end
